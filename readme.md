f0b4c07ed984f0b02bc7002d482f1f5c5529772f
Monarchdos <monarchdosw@gmail.com>
2024-09-20 18:06:39 +0800

    🛠 Fixing known issues.

--- readme.md
+++ readme.md
@@ -1,13 +1,15 @@
 <h1 align="center">_✨ 心灵鸡汤 ✨_</h1>
 <p align="center">
 <a href="https://www.npmjs.com/package/koishi-plugin-soup">
-    <img src="https://img.shields.io/npm/v/koishi-plugin-soup?style=flat-square" alt="pypi">
+    <img src="https://img.shields.io/npm/v/koishi-plugin-soup?style=flat-square" alt="npm">
 </a>
 </p>
 
 ## 📖 介绍
 
 来一碗心灵鸡汤吧。
+
+💭 本插件不支持沙盒调试。
 
 ## 🎉 使用
 
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-import { Context, Schema } from 'koishi'
+import { Context, h, Schema } from 'koishi'
 
 export const name = 'soup'
 
-export function apply(ctx: Context) {
+export interface Config {
+  SoupReplyquote: boolean
+}
+
+export const Config: Schema<Config> = Schema.object({
+  SoupReplyquote: Schema.boolean().default(true).description('是否在回复时引用用户消息'),
+})
+
+export function apply(ctx: Context, config: Config) {
   const jt  = "68747470733a2f2f6170692e61796672652e636f6d2f6a743f747970653d6b6f69736869267665723d312e302e30";
   const djt = "68747470733a2f2f6170692e61796672652e636f6d2f646a743f747970653d6b6f69736869267665723d312e302e30";
   ctx.middleware(async (session, next) => {
@@ -11,7 +19,7 @@ export function apply(ctx: Context) {
       try {
         const res = await ctx.http.get(Buffer.from(jt, 'hex').toString());
         if (res.includes("wwwroot") || res.includes("html") || res.length === 1) return;
-        return res;
+        return (config.SoupReplyquote ? h('quote', { id: session.messageId }) : '') + res;
       } catch (error) {
         ctx.logger('soup').warn('Server connection failed.');
       }
@@ -20,7 +28,7 @@ export function apply(ctx: Context) {
       try {
         const res = await ctx.http.get(Buffer.from(djt, 'hex').toString());
         if (res.includes("wwwroot") || res.includes("html") || res.length === 1) return;
-        return res;
+        return (config.SoupReplyquote ? h('quote', { id: session.messageId }) : '') + res;
       } catch (error) {
         ctx.logger('soup').warn('Server connection failed.');
       }
